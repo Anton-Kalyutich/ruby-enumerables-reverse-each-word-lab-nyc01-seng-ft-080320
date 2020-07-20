16dd3f11a914c7bf7fda792c1435d5e781962cc8
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-20 20:00:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(sentence)
   reverse_array = sentence.split(" ").collect {|word| word.reverse}
-  reverse_sentence = reverse_array.join(" ")
-  return reverse_sentence
+  reverse_array.join(" ")
 end
